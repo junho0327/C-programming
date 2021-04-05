1c54bdcfa612a8befd106fbd9f5e58bd82fce2ea
junho0327 <80371892+junho0327@users.noreply.github.com>
2021-04-05 12:15:03 +0900

    Add files via upload

---
+++ 3_예제6.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("int 형의 크기\t\t\t ==> %d \n", sizeof(int));
+	printf("unsigned int 형의 크기\t\t ==> %d \n", sizeof(unsigned int));
+	printf("short 형의 크기\t\t\t ==> %d \n", sizeof(short));
+	printf("unsigned int 형의 크기\t\t ==> %d \n", sizeof(unsigned int));
+	printf("long int 형의 크기\t\t ==> %d \n", sizeof(long int));
+	printf("unsigned long int 형의 크기\t ==> %d \n", sizeof(unsigned long int));
+	printf("float 형의 크기\t\t\t ==> %d \n", sizeof(float));
+	printf("double 형의 크기\t\t ==> %d \n", sizeof(double));
+	printf("long double 형의 크기\t\t ==> %d \n", sizeof(long double));
+	printf("char 형의 크기\t\t\t ==> %d \n", sizeof(char));
+	printf("unsigned char 형의 크기\t\t ==> %d \n", sizeof(unsigned char));
+
+}
